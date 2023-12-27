9d50f3f1ffa5575db47ab4b552e4c9a21886302f
abdulrahim Ramadan <147656627+abdulrahim-ramadan@users.noreply.github.com>
2023-12-27 04:13:29 +0100

    add admin customizations

--- posts/admin.py
+++ posts/admin.py
@@ -3,4 +3,10 @@ from django.contrib import admin
 from .models import Post
 
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id','name','created_at']
+    list_filter =['created_at']
+    search_fields =['name','content']
+
+
+admin.site.register(Post,PostAdmin)
--- posts/models.py
+++ posts/models.py
@@ -28,3 +28,6 @@ class Post(models.Model):
     name = models.CharField(max_length=100)
     content = models.TextField(max_length=10000)
     created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.name
