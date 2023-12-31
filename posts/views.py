76a0e72288c90b7a36a2aa9ef58102b9983605f2
abdulrahim Ramadan <147656627+abdulrahim-ramadan@users.noreply.github.com>
2023-12-31 19:04:48 +0100

    add post_detail

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from .models import Post
-# Create your views here.
 
 '''
 view :
@@ -9,13 +8,13 @@ view :
     - template
 '''
 
-
-
-
-
-
-
 def post_list(request):
     data = Post.objects.all()    # model
     return render(request,'posts_list.html',{'posts':data})
+
+
+
+def post_detail(request,id):
+    data = Post.objects.get(id=id)
+    return render(request,'post_detail.html',{'post':data})
 
