66b8fd8d6805962a0ea953c076a26c3202e81287
abdulrahim Ramadan <147656627+abdulrahim-ramadan@users.noreply.github.com>
2023-12-31 18:17:37 +0100

    add post_list

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
+
+'''
+view :
+    - url
+    - model
+    - template
+'''
+
+
+
+
+
+
+
+def post_list(request):
+    data = Post.objects.all()    # model
+    return render(request,'posts_list.html',{'posts':data})
+
